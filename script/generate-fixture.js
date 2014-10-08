827fe74fd1461b180131996f58968979e414ffb8
wooorm <tituswormer@gmail.com>
2014-10-08 11:15:07 +0200

    Add script to generate fixtures

---
+++ script/generate-fixture.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/**
+ * Dependencies.
+ */
+
+var ParseDutch,
+    fs;
+
+ParseDutch = require('../');
+fs = require('fs');
+
+/**
+ * `ParseDutch`.
+ */
+
+var dutch;
+
+dutch = new ParseDutch();
+
+/**
+ * Exit with info on too-few parameters.
+ */
+
+var parameters,
+    filepath,
+    nlcst;
+
+parameters = process.argv.splice(2);
+
+if (parameters.length < 2) {
+    console.log('Usage:');
+    console.log('  npm run fixture name document');
+    process.exit(1);
+}
+
+filepath = 'spec/fixture/' + parameters[0] + '.json';
+nlcst = dutch.parse(parameters[1]);
+
+/**
+ * Write fixture.
+ */
+
+fs.writeFileSync(filepath, JSON.stringify(nlcst, 0, 2));
+
+console.log('Wrote file to `' + filepath + '`');
